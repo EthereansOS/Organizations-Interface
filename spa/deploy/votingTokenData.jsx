8e2e2d5301b8a240d3c93a0108cd6661ecfc16b0
Marco Vasapollo <ceo@metaring.com>
2020-06-02 00:21:31 +0200

    Calculating Tokens Amount for Hub

--- spa/deploy/votingTokenData.jsx
+++ spa/deploy/votingTokenData.jsx
@@ -1,6 +1,7 @@
 var VotingTokenData = React.createClass({
     getData() {
-        var data = window.getData(this.domRoot);
+        var _this = this;
+        var data = window.getData(_this.domRoot);
         return (new Promise(function (ok, ko) {
             var errors = [];
             !data.tokenSymbol && errors.push('Insert a valid Token Symbol');
@@ -9,11 +10,27 @@ var VotingTokenData = React.createClass({
                 return ko(errors);
             }
             data.totalSupplyWei = window.toDecimals(data.tokenTotalSupply, 18);
-            window.blockchainCall(window.dfoHub.dFO.methods.read, 'getVotingTokenAmountForHub', window.web3.eth.abi.encodeParameter('uint256', data.totalSupplyWei)).then(result => {
-                data.availableSupply = parseFloat(window.fromDecimals(parseInt(data.totalSupplyWei) - parseInt(window.web3.eth.abi.decodeParameter('uint256', result)), 18));
+            _this.retrieveVotingTokensAmountForHub(data.tokenTotalSupply).then(result => {
+                data.availableSupply = result;
                 return ok(data);
             });
         }));
+    },
+    retrieveVotingTokensAmountForHub(totalSupply) {
+        var totalSupplyWei = window.toDecimals(totalSupply, 18);
+        return window.blockchainCall(window.dfoHub.dFO.methods.read, 'getVotingTokenAmountForHub', window.web3.eth.abi.encodeParameter('uint256', totalSupplyWei)).then(result => {
+            return parseFloat(window.fromDecimals(parseInt(totalSupplyWei) - parseInt(window.web3.eth.abi.decodeParameter('uint256', result)), 18));
+        });
+    },
+    onChange(e) {
+        e && e.preventDefault && e.preventDefault(true) && e.stopPropagation && e.stopPropagation(true);
+        var _this = this;
+        _this.tokenSymbolLabel.innerHTML = _this.tokenSymbol.value;
+        var totalSupply = parseInt(_this.totalSupply.value);
+        totalSupply = isNaN(totalSupply) ? 0 : totalSupply;
+        _this.retrieveVotingTokensAmountForHub(totalSupply).then(result => {
+            _this.amountForHub.innerHTML = window.fromDecimals(window.web3.utils.toBN(window.toDecimals(totalSupply, 18)).sub(window.web3.utils.toBN(window.toDecimals(result, 18))).toString(), 18);
+        });
     },
     render() {
         return (<section>
@@ -21,12 +38,12 @@ var VotingTokenData = React.createClass({
              <section className="DeployNewWhat">
                     <div className="InsertTokenName">
                         <label htmlFor="tokenSymbol">Symbol:</label>
-                        <input autocomplete="off" id="tokenSymbol" type="text" />
+                        <input ref={ref => this.tokenSymbol = ref} autocomplete="off" id="tokenSymbol" type="text" onChange={this.onChange}/>
                     </div>
                     <div className="InsertTokenSupply">
                         <label htmlFor="tokenTotalSupply">Supply:</label>
-                        <input id="tokenTotalSupply" type="number" min="1" />
-                        <aside><b>Generation Fee:</b> 1.5% <span>(10000 UDCF)</span></aside>
+                        <input id="tokenTotalSupply" type="number" min="1" ref={ref => this.totalSupply = ref} onChange={this.onChange}/>
+                        <aside><b>Generation Fee:</b> 1.5% <span>(<span ref={ref => this.amountForHub = ref}>0</span> <span ref={ref => this.tokenSymbolLabel = ref}></span>)</span></aside>
                     </div>
             </section>
         </section>);
