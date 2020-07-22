1349baa0947798216332ddb84ce1bdb51cd45511
alet89 <alessandrotoschi@me.com>
2020-07-22 11:19:24 +0200

    update wallet

--- spa/token/view.jsx
+++ spa/token/view.jsx
@@ -57,55 +57,6 @@ var Token = React.createClass({
             }
         }
     },
-    renderIndexChanger() {
-        return (
-            <ul>
-                <li>
-                    <p>DFOs Front-End is designed to work in two layers, the "Distributed Layer" and the "Decentralized Layer." The Distributed Layer is a version of the Front-End deployed via IPFS or Swarn, for fast and free updates. The Decentralized Layer is a version of the Front-End via On-Chain files, expensive but critical to making a DFO Censorship Resistant. The ENS automatically redirects users to the Distributed Layer and if it's Censored to the Decentralized Layer.</p>
-                </li>
-                <li>
-                    <section>
-                        <a href={this.props.element.link ? "javascript:;" : undefined} className={!this.props.element.link ? undefined : "LinkVisualButton" + (this.state && this.state.indexShow === 'link' ? ' Editing' : "")} onClick={() => this.props.element.link && this.setState({ indexShow: this.state && this.state.indexShow === 'link' ? null : 'link' })}>Code</a>
-                        <input id="linkCheck" type="checkbox" className="DFOFunctionIndexIconSelector" ref={ref => this.linkCheck = ref} onChange={e => { this.linkValue.value = this.props.element.link || ''; this.linkValue.disabled = !e.currentTarget.checked; this.indexCheck.checked = false; this.indexFile.disabled = true; }} />
-                        <label htmlFor="linkCheck">Distributed Layer</label>
-                        <input className="DFOFunctionIndexIconText" id="link" type="text" placeholder="Link IPFS Swarm" ref={ref => (this.linkValue = ref) && (ref.value = this.props.element.link || '')} disabled />
-                    </section>
-                    <section>
-                        <a href={this.props.element.index ? 'javascript:;' : undefined} className={!this.props.element.index ? undefined : "LinkVisualButton" + (this.state && this.state.indexShow === 'index' ? ' Editing' : "")} onClick={() => this.props.element.index && this.props.element.index !== '0' && this.setState({ indexShow: this.state && this.state.indexShow === 'index' ? null : 'index' })}>Code</a>
-                        <input type="hidden" id="index" ref={ref => (this.indexValue = ref) && (ref.value = this.props.element.index)} />
-                        <input id="indexCheck" type="checkbox" className="DFOFunctionIndexIconSelector" ref={ref => this.indexCheck = ref} onChange={e => { this.indexValue.value = this.props.element.index; this.indexFile.value = ''; this.indexFile.disabled = !e.currentTarget.checked; this.linkCheck.checked = false; this.linkValue.disabled = true; }} />
-                        <label htmlFor="indexCheck">Decentralized Layer</label>
-                        <input className="DFOFunctionIndexIconUpload" id="indexFile" type="file" accept=".html,.htm" ref={ref => this.indexFile = ref} disabled onChange={this.uploadFile} />
-                    </section>
-                </li>
-                {this.state && this.state.indexShow && <section>
-                    {this.state.indexShow === 'index' && <Editor lang="html" readonly="true" first={this.state.indexShow === 'index' ? this.props.element.index : undefined} />}
-                    {this.state.indexShow === 'link' && <Editor lang="html" readonly="true" link={this.state.indexShow === 'link' ? this.props.element.link : undefined} />}
-                </section>}
-            </ul>
-        );
-    },
-    renderSurveySingleRewardChanger() {
-        return this.renderDefaultChanger("The number of Voting Tokens as a reward to the issuer for every single Successful Proposal.", "Dev Incentives", "surveySingleReward", window.fromDecimals(this.props.element.surveySingleReward, this.props.element.decimals));
-    },
-    renderProposalLengthChanger() {
-        return this.renderDefaultChanger("Every survey has a length expressed in Blocks. Here you can set the duration of Surveys for this DFO.", "Proposal Length", "proposalLength", this.props.element.blocks);
-    },
-    renderEmergencyLengthChanger() {
-        return this.renderDefaultChanger("The length in Blocks for Emergency Surveys.", "Emergency Length", "minimumBlockNumberForEmergencySurvey", this.props.element.minimumBlockNumberForEmergencySurvey);
-    },
-    renderEmergencyPenaltyChanger() {
-        return this.renderDefaultChanger("The Fee that Emergency Proposal Issuer must stake to propose it. This stake will be lost if the Proposal fails.", "Emergency Penalty", "emergencySurveyStaking", window.fromDecimals(this.props.element.emergencySurveyStaking, this.props.element.decimals));
-    },
-    renderQuorumChanger() {
-        return this.renderDefaultChanger("The Quorum is a minimum number of Voting Tokens staked in a survey to reach the success status.", "Quorum", "quorum", window.fromDecimals(this.props.element.quorum, this.props.element.decimals));
-    },
-    renderProposalStakeChanger() {
-        return this.renderDefaultChanger("The minimum of Token Stacked needed to create a new Proposal.", "Proposal Stake", "minimumStaking", window.fromDecimals(this.props.element.minimumStaking, this.props.element.decimals));
-    },
-    renderVotesHardCapChanger() {
-        return this.renderDefaultChanger('If a proposal reaches a fixed number of voting tokens (example the 90% of the total Token supply) for "Approve" or "Disapprove" it, the proposal automatically ends, independently from the duration rule.', "Hard Cap", "votesHardCap", window.fromDecimals(this.props.element.votesHardCap, this.props.element.decimals));
-    },
     renderDefaultChanger(text, label, id, defaultValue) {
         return (
             <ul>
@@ -127,70 +78,49 @@ var Token = React.createClass({
             <section className="DFOOverview">
                 <ul className="DFOHosting">
                     <section className="HostingCategoryTitle">
-                        <h2>Decentralized Application</h2>
+                        <h2>Voting Token</h2>
                     </section>
-                    <li className="TheDappInfo1">
-                        <h5 className="DFOHostingTitle">&#128587; Voting Token</h5>
+                    <li className="TheDappInfo05">
                         <section className="DFOTitleSection">
+                        <h5 className="DFOHostingTitle">Ticker:</h5>
                             <AsyncValue>
-                                {_this.props.element.symbol && <p className="DFOLabelTitleInfo">Ticker: <a className="LinkVisualStandard" href={window.getNetworkElement("etherscanURL") + 'token/' + _this.props.element.token.options.address} target="_blank">{_this.props.element.symbol}</a></p>}
+                                {_this.props.element.symbol && <p className="DFOLabelTitleInfo"><a className="LinkVisualStandard" href={window.getNetworkElement("etherscanURL") + 'token/' + _this.props.element.token.options.address} target="_blank">{_this.props.element.symbol}</a></p>}
                             </AsyncValue>
                         </section>
                         <section className="DFOTitleSection">
-                            <AsyncValue>
-                                {_this.props.element.symbol && _this.props.element.totalSupply && <p className="DFOLabelTitleInfo">Supply: <a className="LinkVisualStandard" href={window.getNetworkElement("etherscanURL") + 'token/tokenholderchart/' + _this.props.element.token.options.address} target="_blank">{window.fromDecimals(_this.props.element.totalSupply, _this.props.element.decimals)}</a></p>}
-                            </AsyncValue>
                         </section>
-                        <section className="DFOTitleSection">
+                        {/*<section className="DFOTitleSection">
                             <a href={window.context.uniSwapInfoURL + this.props.element.token.options.address} target="_blank" className="LinkVisualButton LinkVisualUni">&#129412; Info</a>
                             {"\u00a0"}
                             <a href={window.context.uniSwapSwapURL + this.props.element.token.options.address} target="_blank" className="LinkVisualButton LinkVisualUni">&#129412; Swap</a>
-                        </section>
+                        </section> */}
                     </li>
                     <li className="TheDappInfo1">
-                    <h5 className="DFOHostingTitle">&#128302; Domain <a className={"EditDFOYoYO" + (_this.dfoElement && _this.dfoElement.state && _this.dfoElement.state.okBoomer ? ' Editing' : '')} href="javascript:;" onClick={() => _this.emit('okBommer/toggle')}>i</a></h5>
-                        <section className="DFOTitleSection">
+                    <section className="DFOTitleSection">
+                        <h5 className="DFOHostingTitle">Existing Supply:  </h5>
+                        <AsyncValue>
+                                {_this.props.element.symbol && _this.props.element.totalSupply && <p className="DFOLabelTitleInfo"><a className="LinkVisualStandard" href={window.getNetworkElement("etherscanURL") + 'token/tokenholderchart/' + _this.props.element.token.options.address} target="_blank">{window.fromDecimals(_this.props.element.totalSupply, _this.props.element.decimals)}</a></p>}
+                        </AsyncValue>
+                        <h5 className="DFOHostingTitle">DFO Wallet Supply: </h5>
                             <AsyncValue>
-                                {_this.props.element.ens !== undefined && <p className="DFOLabelTitleInfo">ENS: <a className="LinkVisualStandard" target="_blank" href={"https://" + ((_this.props.element.ens && (_this.props.element.ens.toLowerCase() + '.')) || '') + "dfohub.eth?ensd=" + ((_this.props.element.ens && (_this.props.element.ens.toLowerCase() + '.')) || '') + "dfohub.eth"}>{(_this.props.element.ens && (_this.props.element.ens.toLowerCase() + '.')) || ''}dfohub.eth</a></p>}
+                                {_this.props.element.symbol && _this.props.element.totalSupply && <p className="DFOLabelTitleInfo"> <a className="LinkVisualStandard" href={window.getNetworkElement("etherscanURL") + 'token/tokenholderchart/' + _this.props.element.token.options.address} target="_blank">{window.fromDecimals(_this.props.element.totalSupply, _this.props.element.decimals)}</a></p>}
                             </AsyncValue>
-                        </section>
-                        <OkBoomer okBoomer={_this.props.okBoomer}>A perpetual unique Web3-Based Name Service to reach this Application</OkBoomer>
+                    </section>
                     </li>
                     <li className="TheDappInfo05">
-                    <h5 className="DFOHostingTitle DFOHostingTitle2">&#128736; Functions </h5>
                         <section className="DFOTitleSection">
-                            <a className="LinkVisualStandard" href="javascript:;" onClick={() => _this.emit('section/change', 'Functions')}>
-                                <AsyncValue>
-                                    {_this.props.element.functionalitiesAmount}
-                                </AsyncValue>
-                            </a>
-                        </section>
-                    </li>
-                    <li className="TheDappInfo05">
-                    <h5 className="DFOHostingTitle">&#128242; Front-end <a className={"EditDFOYoYO" + (_this.dfoElement && _this.dfoElement.state && _this.dfoElement.state.okBoomer ? ' Editing' : '')} href="javascript:;" onClick={() => _this.emit('okBommer/toggle')}>i</a></h5>
-                        <section className="DFOTitleSection">
+                        <h5 className="DFOHostingTitle">Power:</h5>
                             <AsyncValue>
-                                {<a className="LinkVisualButton" href="javascript:;" onClick={() => (_this.props.element.link || _this.props.element.index) && this.setState({ change: this.state && this.state.change === 'showCode' ? null : 'showCode' })}>Code</a>}
+                                +1
                             </AsyncValue>
-                            {_this.renderChangeButton('index')}
                         </section>
-                        <OkBoomer okBoomer={_this.props.okBoomer}>The Front-End code of the Application</OkBoomer>
+                        <section className="DFOTitleSection">
+                        </section>
                     </li>
-                    {_this.renderChanger(['index'])}
-                    {this.state && this.state.change === 'showCode' && <section className="IndexShowCode">
-                        <section className="IndexShowCodeEditorBox">
-                            <h5>Distributed Layer</h5>
-                            <Editor lang="html" readonly="true" link={_this.props.element.link} firstCode={_this.props.element.link ? undefined : "Distributed Index is actually unset"} />
-                        </section>
-                        <section className="IndexShowCodeEditorBox">
-                            <h5>Decentralized Layer</h5>
-                            <Editor lang="html" readonly="true" first={_this.props.element.index} firstCode={_this.props.element.index ? undefined : "Decentralized Index is actually unset"} />
-                        </section>
-                    </section>}
                 </ul>
                 <ul className="DFOHosting">
                 <section className="HostingCategoryTitle">
-                    <h2>Governance Rules</h2>
+                    <h2>DEX Liquidity</h2>
                 </section>
                 <li className="TheDappInfo2">
                         <h5 className="DFOHostingTitle">&#127984; Regular Proposals: <a className={"EditDFOYoYO" + (_this.dfoElement && _this.dfoElement.state && _this.dfoElement.state.okBoomer ? ' Editing' : '')} href="javascript:;" onClick={() => _this.emit('okBommer/toggle')}>i</a></h5>
