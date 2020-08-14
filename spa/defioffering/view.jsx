e2c9e8d9af989e85faf86dfc9327ce4d655f15a4
Marco Vasapollo <ceo@metaring.com>
2020-08-14 18:37:35 +0200

    Defi Offering Loader

--- spa/defioffering/view.jsx
+++ spa/defioffering/view.jsx
@@ -4,25 +4,35 @@ var DeFiOffering = React.createClass({
         'spa/okBoomer.jsx',
         'spa/loaderMinimino.jsx',
         'spa/defioffering/fixedInflation/view.jsx',
-        'spa/defioffering/staking/view.jsx'
+        'spa/defioffering/staking/view.jsx',
+        'spa/loaderMini.jsx'
     ],
     getDefaultSubscriptions() {
         return {
-            'staking/refresh' : this.controller.loadStakingData,
-            'fixedInflation/refresh' : this.controller.loadFixedInflationData
+            'staking/refresh': this.controller.loadStakingData,
+            'fixedInflation/refresh': this.controller.loadFixedInflationData
         };
     },
     componentDidMount() {
-        this.controller.loadFixedInflationData();
-        this.controller.loadStakingData();
+        var _this = this;
+        _this.setState({ cumulativeLoading: true }, function () {
+            Promise.all([
+                _this.controller.loadFixedInflationData(),
+                _this.controller.loadStakingData()
+            ]
+            ).then(function() {
+                _this.setState({cumulativeLoading : false})
+            });
+        });
     },
     render() {
         var props = {};
         this.props && Object.entries(this.props).forEach(entry => props[entry[0]] = entry[1]);
         this.state && Object.entries(this.state).forEach(entry => props[entry[0]] = entry[1]);
         return (<section className="DFOOverview">
-            {React.createElement(StakingView, props)}
-            {React.createElement(FixedInflationView, props)}
+            {(!this.state || this.state.cumulativeLoading) && [<br/>, <br/>, <LoaderMini/>]}
+            {this.state && this.state.cumulativeLoading === false && React.createElement(StakingView, props)}
+            {this.state && this.state.cumulativeLoading === false && React.createElement(FixedInflationView, props)}
         </section>);
     }
 });
