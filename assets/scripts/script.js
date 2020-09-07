40757b17d17cb8a7c950a9bdc9be543fed6d30e1
Marco Vasapollo <ceo@metaring.com>
2020-09-07 17:37:17 +0200

    No checks

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1782,35 +1782,43 @@ window.uploadToIPFS = async function uploadToIPFS(files) {
 window.validateDFOMetadata = async function validateDFOMetadata(metadata, noUpload) {
     var errors = [];
     !metadata && errors.push('Please provide data');
+    try {
+        metadata.brandUri = metadata.brandUri.item(0);
+    } catch(e) {
+    }
     if(metadata && typeof metadata.brandUri !== 'string' && !await window.checkCoverSize(metadata.brandUri)) {
-        errors.push('Brand Logo must be valid 320x320 image');
+        //errors.push('Brand Logo must be valid 320x320 image');
     }
     try {
         metadata && (metadata.brandUri = (!metadata.brandUri || noUpload) ? metadata.brandUri : (typeof metadata.brandUri === 'string' && metadata.brandUri.indexOf('ipfs') !== -1) ? metadata.brandUri : await window.uploadToIPFS(metadata.brandUri));
     } catch(e) {
         errors.push(e.message || e);
     }
+    try {
+        metadata.logoUri = metadata.logoUri.item(0);
+    } catch(e) {
+    }
     if(metadata && typeof metadata.logoUri !== 'string' && !await window.checkCoverSize(metadata.logoUri)) {
-        errors.push('Token Logo must be valid 320x320 image');
+        //errors.push('Token Logo must be valid 320x320 image');
     }
     try {
         metadata && (metadata.logoUri = (!metadata.logoUri || noUpload) ? metadata.logoUri : (typeof metadata.logoUri === 'string' && metadata.logoUri.indexOf('ipfs') !== -1) ? metadata.logoUri : await window.uploadToIPFS(metadata.logoUri));
     } catch(e) {
-        errors.push(e.message || e);
+        //errors.push(e.message || e);
     }
 
-    metadata && !metadata.name && errors.push("Name is mandatory in metadata");
-    metadata && !metadata.shortDescription && errors.push("BIO is mandatory in metadata");
-    metadata && (!metadata.wpUri || !new RegExp(window.urlRegex).test(metadata.wpUri)) && errors.push("Explainer link must contain a valid URL");
-    metadata && !noUpload && (!metadata.brandUri || !new RegExp(window.urlRegex).test(metadata.brandUri)) && errors.push("DFO Logo is not a valid URL");
-    metadata && !noUpload && (!metadata.logoUri || !new RegExp(window.urlRegex).test(metadata.logoUri)) && errors.push("Token Logo is not a valid URL");
-    metadata && noUpload && !metadata.brandUri && errors.push("Insert a valid DFO Logo");
-    metadata && noUpload && !metadata.logoUri && errors.push("Insert a valid Token Logo image");
-    metadata && (!metadata.discussionUri || !new RegExp(window.urlRegex).test(metadata.discussionUri)) && errors.push("Chat link must contain a valid URL");
-    metadata && (!metadata.repoUri || !new RegExp(window.urlRegex).test(metadata.repoUri)) && errors.push("Repo link must contain a valid URL");
-    metadata && (!metadata.externalDNS || !new RegExp(window.urlRegex).test(metadata.externalDNS)) && errors.push("External link must contain a valid URL");
-    metadata && (!metadata.externalENS || !new RegExp(window.urlRegex).test(metadata.externalENS) || metadata.externalENS.indexOf('.eth') === -1) && errors.push("External ENS link must contain a valid ENS URL");
-    metadata && (!metadata.roadmapUri || !new RegExp(window.urlRegex).test(metadata.roadmapUri)) && errors.push("Roadmap link must contain a valid URL");
+    //metadata && !metadata.name && errors.push("Name is mandatory in metadata");
+    //metadata && !metadata.shortDescription && errors.push("BIO is mandatory in metadata");
+    //metadata && (!metadata.wpUri || !new RegExp(window.urlRegex).test(metadata.wpUri)) && errors.push("Explainer link must contain a valid URL");
+    //metadata && !noUpload && (!metadata.brandUri || !new RegExp(window.urlRegex).test(metadata.brandUri)) && errors.push("DFO Logo is not a valid URL");
+    //metadata && !noUpload && (!metadata.logoUri || !new RegExp(window.urlRegex).test(metadata.logoUri)) && errors.push("Token Logo is not a valid URL");
+    //metadata && noUpload && !metadata.brandUri && errors.push("Insert a valid DFO Logo");
+    //metadata && noUpload && !metadata.logoUri && errors.push("Insert a valid Token Logo image");
+    //metadata && (!metadata.discussionUri || !new RegExp(window.urlRegex).test(metadata.discussionUri)) && errors.push("Chat link must contain a valid URL");
+    //metadata && (!metadata.repoUri || !new RegExp(window.urlRegex).test(metadata.repoUri)) && errors.push("Repo link must contain a valid URL");
+    //metadata && (!metadata.externalDNS || !new RegExp(window.urlRegex).test(metadata.externalDNS)) && errors.push("External link must contain a valid URL");
+    //metadata && (!metadata.externalENS || !new RegExp(window.urlRegex).test(metadata.externalENS) || metadata.externalENS.indexOf('.eth') === -1) && errors.push("External ENS link must contain a valid ENS URL");
+    //metadata && (!metadata.roadmapUri || !new RegExp(window.urlRegex).test(metadata.roadmapUri)) && errors.push("Roadmap link must contain a valid URL");
     if(errors.length > 0) {
         throw errors.join('\n');
     }
