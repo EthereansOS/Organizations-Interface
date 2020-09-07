0bfabf0e9b8d4e7d16ed2a1aa2d87e3240ff4687
alet89 <alessandrotoschi@me.com>
2020-09-07 18:01:37 +0200

    general improvements

--- spa/dFOMetadata/edit.jsx
+++ spa/dFOMetadata/edit.jsx
@@ -43,42 +43,52 @@ var DFOMetadataEdit = React.createClass({
                     <label className="LabelSPEC" htmlFor="shortDescription">BIO:</label>
                     <textarea className="LabelSPECWRITE" id="shortDescription" type="text"></textarea>
                 </div>
+                <p className="OkBoomer">A brief description of the organization <div className="BoomerTriangle"/></p>
                 <div className="InsertDfoName">
                     <label htmlFor="name">DFO Name:</label>
                     <input autocomplete="off" id="name" type="text" />
                 </div>
+                <p className="OkBoomer">The name of the organization<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="brandUri">DFO Logo:</label>
                     <input id="brandUri" type="file" accepts="image/*" />
                 </div>
+                <p className="OkBoomer">The logo of the organization (must be a .png 350 x 350 pixels)<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="logoUri">Token Logo:</label>
                     <input id="logoUri" type="file" accepts="image/*" />
                 </div>
+                <p className="OkBoomer">The logo of the Voting Token (must be a .png 350 x 350 pixels)<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="discussionUri">Chat Link:</label>
                     <input id="discussionUri" type="text" />
                 </div>
+                <p className="OkBoomer">A link to the official community of the organization (ex. Riot, Discord, Telegram)<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="repoUri">Repo link:</label>
                     <input id="repoUri" type="text" />
                 </div>
+                <p className="OkBoomer">A link to the official R&D repository<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="wpUri">Explainer link:</label>
                     <input id="wpUri" type="text" />
                 </div>
+                <p className="OkBoomer">A link to an external source that explain the plan of the organization<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="roadmapUri">Roadmap link:</label>
                     <input id="roadmapUri" type="text" />
                 </div>
+                <p className="OkBoomer">A link to an external source that provide the roadmap of the project<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="externalDNS">External link:</label>
                     <input id="externalDNS" type="text" />
                 </div>
+                <p className="OkBoomer">A link to an external webpage to use the application (if any).<div className="BoomerTriangle"/></p>
                 <div className="InsertDfoSubdomain">
                     <label htmlFor="externalENS">External ENS link:</label>
                     <input id="externalENS" type="text" />
                 </div>
+                <p className="OkBoomer">A link to an external ENS webpage to use the application (if any).<div className="BoomerTriangle"/></p>
             </section>
             {this.props.showCommands && <section>
                 <br/>
