e08b161770c825dfef501d3e4be2ff9b3bcb1037
Marco Vasapollo <ceo@metaring.com>
2020-09-26 17:13:12 +0200

    Select instead of checkbox

--- spa/farm/quickScope/quickScope/view.jsx
+++ spa/farm/quickScope/quickScope/view.jsx
@@ -16,36 +16,24 @@ var QuickScope = React.createClass({
     },
     sortList() {
         var list = (this.state && this.state.list) || [];
-        var renderedList = (this.state && this.state.options) ? [] : list;
-        if(this.state && this.state.options) {
-            for(var pair of list) {
-                if(this.state.options[pair.token0.address].selected && this.state.options[pair.token1.address].selected) {
-                    renderedList.push(pair);
-                }
-            }
+        if(!this.state || !this.state.selected || this.state.selected === 'all') {
+            return list;
         }
-        return renderedList;
+        return list.filter(it => it.token0.address === this.state.selected || it.token1.address === this.state.selected);
     },
     onSelection(e) {
-        var options = this.state.options;
-        options[e.currentTarget.dataset.key].selected = !options[e.currentTarget.dataset.key].selected;
-        this.setState({options});
+        this.setState({selected: e.currentTarget.value});
     },
     render() {
-        var _this = this;
         var tokenT = window.wethToken;
         return (<section>
             {(!this.state || !this.state.list) && <LoaderMini />}
-            {this.state && this.state.options && <ul>
-                {Object.values(this.state.options).map(it => <li key={it.token.address}>
-                    <label>
-                        <input type="checkbox" data-key={it.token.address} onChange={_this.onSelection} checked={it.selected}/>
-                        <img src={it.token.logo}/>
-                        <span>{it.token.name}</span>
-                        <span>({it.token.name})</span>
-                    </label>
-                </li>)}
-            </ul>}
+            {this.state && this.state.options && <select onChange={this.onSelection}>
+                <option value="all">All</option>
+                {Object.values(this.state.options).map(it => <option value={it.token.address} key={it.token.address}>
+                    Only {it.token.name} ({it.token.symbol})
+                </option>)}
+            </select>}
             {this.state && this.state.list && <ul>
                 {this.sortList().map(it => <li key={it.key}>
                     <PairToken lock={this.state && this.state.lock} pair={it} selection="token0" tokenT={tokenT}/>
